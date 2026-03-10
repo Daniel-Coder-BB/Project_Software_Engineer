2ef244d58df99b783947b7ef43ba0ce5143159d0
Daniel-Coder-BB <dannyluango48@gmail.com>
2026-03-10 14:02:52 +0100

    added get and set methods to Room.

--- Classes/Room/Room.cpp
+++ Classes/Room/Room.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Room.h"
+
+string Room::get_name() const {
+    return name;
+}
+
+void Room::set_name(const string &name) {
+    this->name = name;
+}
+
+string Room::get_identifier() const {
+    return identifier;
+}
+
+void Room::set_identifier(const string &identifier) {
+    this->identifier = identifier;
+}
+
+unsigned int Room::get_capacity() const {
+    return capacity;
+}
+
+void Room::set_capacity(unsigned int capacity) {
+    this->capacity = capacity;
+}
