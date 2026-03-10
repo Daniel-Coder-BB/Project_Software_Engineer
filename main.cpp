947631fc95bbca12825d63b62dde7d88a5adc61e
Daniel-Coder-BB <dannyluango48@gmail.com>
2026-03-10 15:34:10 +0100

    added functions to parse room,meetings and participations

--- main.cpp
+++ main.cpp
@@ -1,26 +1,62 @@
 #include <iostream>
 #include "tinyxml.h"
-#include "CD.h"
+#include "Classes/Room/Room.h"
+#include "Classes/Meeting/Meeting.h"
 #include <gtest/gtest.h>
 #include <chrono>
 
+#include "Classes/Participation/Participation.h"
+
 using namespace std;
 
-CD parseCDElement(TiXmlElement* cdElem) {
-    CD newCD;
-    if (cdElem->FirstChildElement("NAME")) {
-        newCD.setartist(cdElem->FirstChildElement("NAME")->GetText());
+Room parse_room_element(TiXmlElement* room_element) {
+    Room new_room;
+    if (room_element->FirstChildElement("NAME")) {
+        new_room.set_name(room_element->FirstChildElement("NAME")->GetText());
     }
-    if (cdElem->FirstChildElement("IDENTIFIER")) {
-        newCD.settitle(cdElem->FirstChildElement("IDENTIFIER")->GetText());
+    if (room_element->FirstChildElement("IDENTIFIER")) {
+        new_room.set_identifier(room_element->FirstChildElement("IDENTIFIER")->GetText());
     }
-    if (cdElem->FirstChildElement("CAPACITY")) {
+    if (room_element->FirstChildElement("CAPACITY")) {
         // Omzetten van string naar int
-        int y = std::stoi(cdElem->FirstChildElement("CAPACITY")->GetText());
-        newCD.setyear(y);
+        int capacity = std::stoi(room_element->FirstChildElement("CAPACITY")->GetText());
+        new_room.set_capacity(capacity);
     }
 
-    return newCD;
+    return new_room;
+}
+
+Meeting parse_meeting_element(TiXmlElement* meeting_element) {
+    Meeting new_meeting;
+    if (meeting_element->FirstChildElement("LABEL")) {
+        new_meeting.set_label(meeting_element->FirstChildElement("LABEL")->GetText());
+    }
+    if (meeting_element->FirstChildElement("IDENTIFIER")) {
+        new_meeting.set_identifier(meeting_element->FirstChildElement("IDENTIFIER")->GetText());
+    }
+    if (meeting_element->FirstChildElement("ROOM")) {
+        new_meeting.set_room(meeting_element->FirstChildElement("ROOM")->GetText());
+    }
+
+    if (meeting_element->FirstChildElement("DATE")) {
+        new_meeting.set_room(meeting_element->FirstChildElement("DATE")->GetText());
+    }
+
+    return new_meeting;
+}
+
+
+Participation parse_participation_element(TiXmlElement* participation_element) {
+    Participation new_participation;
+    if (participation_element->FirstChildElement("LABEL")) {
+        new_participation.set_user(participation_element->FirstChildElement("LABEL")->GetText());
+    }
+    if (participation_element->FirstChildElement("IDENTIFIER")) {
+        new_participation.set_meeting(participation_element->FirstChildElement("IDENTIFIER")->GetText());
+    }
+
+
+    return new_participation;
 }
 
 
@@ -60,8 +96,18 @@ int main(int argc, char **argv) {
 
     // Loop over alle <ROOm> elementen binnen  <SYSTEM> [cite: 57, 58]
     for (TiXmlElement* room = root->FirstChildElement("ROOM"); room != NULL; room = room->NextSiblingElement("ROOM")) {
-        CD mijnCD = parseCDElement(room);
-        mijnCD.print();
+        Room new_room = parse_room_element(room);
+        new_room.print();
+    }
+
+    for (TiXmlElement* meeting = root->FirstChildElement("MEETING"); meeting != NULL; meeting = meeting->NextSiblingElement("MEETING")) {
+        Meeting new_meeting = parse_meeting_element(meeting);
+        new_meeting.print();
+    }
+
+    for (TiXmlElement* participation = root->FirstChildElement("MEETING"); participation != NULL; participation = participation->NextSiblingElement("MEETING")) {
+        Participation new_participation = parse_participation_element(participation);
+        new_participation.print();
     }
 
     doc.Clear();
