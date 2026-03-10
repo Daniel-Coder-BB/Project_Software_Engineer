3ad53cc86f8cfda1cfbb1ab35717457e25e6a725
Daniel-Coder-BB <dannyluango48@gmail.com>
2026-03-10 15:15:31 +0100

    Put the checker if root is NULL and if file opens fails in functions to make it more modular

--- main.cpp
+++ main.cpp
@@ -24,7 +24,12 @@ CD parseCDElement(TiXmlElement* cdElem) {
 }
 
 
-
+/*
+ *This function checks if an error happens with reading the file
+ *@param filename The directory where the file is
+ *@param doc a TiXmlDocument wich contains our XML file
+ *@return if failed (1) or if no errors(0)
+ */
 int file_error_check(const string& filename,TiXmlDocument& doc) {
 
     if (!doc.LoadFile(filename.c_str())) {
@@ -34,21 +39,28 @@ int file_error_check(const string& filename,TiXmlDocument& doc) {
     return 0;
 }
 
+
+int if_root_exists(TiXmlElement* root) {
+    if (root == NULL) {
+        cerr << "Geen root element gevonden." << endl;
+        return 1;
+    }
+    return 0;
+}
+
+
+
 int main(int argc, char **argv) {
     TiXmlDocument doc;
     string filename = "../XML_files/Room.xml";
     file_error_check(filename,doc);
 
-    TiXmlElement* root = doc.FirstChildElement(); // Dit is <CATALOG>
-    if (root == NULL) {
-        cerr << "Geen root element gevonden." << endl;
-        return 1;
-    }
+    TiXmlElement* root = doc.FirstChildElement(); // Dit is <system>
+    if_root_exists(root);
 
-
-    // Loop over alle <CD> elementen binnen de <CATALOG> [cite: 57, 58]
-    for (TiXmlElement* cd = root->FirstChildElement("ROOM"); cd != NULL; cd = cd->NextSiblingElement("ROOM")) {
-        CD mijnCD = parseCDElement(cd);
+    // Loop over alle <ROOm> elementen binnen  <SYSTEM> [cite: 57, 58]
+    for (TiXmlElement* room = root->FirstChildElement("ROOM"); room != NULL; room = room->NextSiblingElement("ROOM")) {
+        CD mijnCD = parseCDElement(room);
         mijnCD.print();
     }
 
