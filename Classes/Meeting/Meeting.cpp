5178b878861f7db3bfb2967fd9184235937a7efd
Daniel-Coder-BB <dannyluango48@gmail.com>
2026-03-10 13:51:04 +0100

    Created Room,meeting and Participation class and deleted file reader class (will just be made a function)

---
+++ Classes/Meeting/Meeting.cpp
@@ -0,0 +1,5 @@
+//
+// Created by danny on 10-Mar-26.
+//
+
+#include "Meeting.h"
---
+++ Classes/Meeting/Meeting.h
@@ -0,0 +1,16 @@
+//
+// Created by danny on 10-Mar-26.
+//
+
+#ifndef MEETING_H
+#define MEETING_H
+
+
+
+class Meeting {
+
+};
+
+
+
+#endif //MEETING_H
