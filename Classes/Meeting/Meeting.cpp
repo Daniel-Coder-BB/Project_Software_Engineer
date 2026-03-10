cb4ee03aed4906281f2aee554b0d2189454353cb
Ibrahim <Ibrabamut95@gmail.com>
2026-03-10 14:33:49 +0100

    Something

--- Classes/Meeting/Meeting.cpp
+++ Classes/Meeting/Meeting.cpp
@@ -1,29 +1,6 @@
 //
-// Created by danny on 10-Mar-26.
+// Created by ibrab on 10.03.2026.
 //
 
 #include "Meeting.h"
 
-string Meeting::get_label() const {
-    return label;
-}
-
-void Meeting::set_label(const string &label) {
-    this->label = label;
-}
-
-string Meeting::get_identifier() const {
-    return identifier;
-}
-
-void Meeting::set_identifier(const string &identifier) {
-    this->identifier = identifier;
-}
-
-string Meeting::get_room() const {
-    return room;
-}
-
-void Meeting::set_room(const string &room) {
-    this->room = room;
-}
