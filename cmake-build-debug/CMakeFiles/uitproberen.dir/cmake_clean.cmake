d7693f4f00f494e4a6cf3f06de3792d7490d5f52
Ibrahim <Ibrabamut95@gmail.com>
2026-03-10 13:54:42 +0100

    ffggffgfg

---
+++ cmake-build-debug/CMakeFiles/uitproberen.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uitproberen.dir/Classes/File_Reader/File_Reader.cpp.obj"
+  "CMakeFiles/uitproberen.dir/Classes/File_Reader/File_Reader.cpp.obj.d"
+  "CMakeFiles/uitproberen.dir/Classes/Room/Room.cpp.obj"
+  "CMakeFiles/uitproberen.dir/Classes/Room/Room.cpp.obj.d"
+  "CMakeFiles/uitproberen.dir/main.cpp.obj"
+  "CMakeFiles/uitproberen.dir/main.cpp.obj.d"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinystr.cpp.obj"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinystr.cpp.obj.d"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinyxml.cpp.obj"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinyxml.cpp.obj.d"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinyxmlerror.cpp.obj"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinyxmlerror.cpp.obj.d"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinyxmlparser.cpp.obj"
+  "CMakeFiles/uitproberen.dir/tinyxml_2_6_2/tinyxml/tinyxmlparser.cpp.obj.d"
+  "libuitproberen.dll.a"
+  "uitproberen.exe"
+  "uitproberen.exe.manifest"
+  "uitproberen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uitproberen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
