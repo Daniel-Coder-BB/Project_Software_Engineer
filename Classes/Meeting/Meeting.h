34813057f338bbd26e992698d057d2b5dc39f04b
Ibrahim <Ibrabamut95@gmail.com>
2026-03-10 14:41:01 +0100

    Something

--- Classes/Meeting/Meeting.h
+++ Classes/Meeting/Meeting.h
@@ -4,21 +4,49 @@
 
 #ifndef UITPROBEREN_MEETING_H
 #define UITPROBEREN_MEETING_H
-
-#include <iostream>
 #include <string>
 #include <ctime>
-using namespace std;
+
 
 class Meeting {
 private:
-    string label;
-    string identifier;
-    string room;
-    tm data{};
+    std::string label;
+    std::string identifier;
+    std::string room;
+    tm data;
 
 public:
+    std::string get_label() const {
+        return label;
+    }
 
+    void set_label(const std::string &new_label) {
+        this->label = new_label;
+    }
+
+    std::string get_identifier() const {
+        return identifier;
+    }
+
+    void set_identifier(const std::string &new_identifier) {
+        this->identifier = identifier;
+    }
+
+    std::string get_room() const {
+        return room;
+    }
+
+    void set_room(const std::string &new_room) {
+        this->room = new_room;
+    }
+
+    tm get_data() const {
+        return data;
+    }
+
+    void set_data(const tm &data) {
+        this->data = data;
+    }
 };
 
 
